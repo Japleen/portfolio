195a7dc43ff817518e5d534ae0ee10d5c9f30f73
Japleen Kaur <japleen.kaur11@gmail.com>
2024-05-03 10:19:50 +0530

    image drag, removed upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.getElementById('file-upload').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-    reader.onload = function(e) {
-      document.getElementById('profile-picture').innerHTML = '<img src="' + e.target.result + '" alt="Profile Picture">';
-    };
-    reader.readAsDataURL(file);
-  });
+// document.getElementById('file-upload').addEventListener('change', function(event) {
+//     const file = event.target.files[0];
+//     const reader = new FileReader();
+//     reader.onload = function(e) {
+//       document.getElementById('profile-picture').innerHTML = '<img src="' + e.target.result + '" alt="Profile Picture">';
+//     };
+//     reader.readAsDataURL(file);
+//   });
   
