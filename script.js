d10a97365096d87720a4527b5d920d366c8b165c
Japleen Kaur <japleen.kaur11@gmail.com>
2024-05-03 09:15:40 +0530

    profile pic

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('file-upload').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      document.getElementById('profile-picture').innerHTML = '<img src="' + e.target.result + '" alt="Profile Picture">';
+    };
+    reader.readAsDataURL(file);
+  });
+  
